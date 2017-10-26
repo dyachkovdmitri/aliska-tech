f5b6715cab0c97f3b6667c3405e580f115cb44bf
d.dyachkov <YVU7ih5G>
2017-10-26 18:00:51 +0300

    connector completed wattage begin

--- src/main/java/com/neiron/neiron/standartValues/StandartWattage.java
+++ src/main/java/com/neiron/neiron/standartValues/StandartWattage.java
@@ -8,10 +8,14 @@ public class StandartWattage {
     public final static HashMap<String, String> standartWattages = new HashMap<>();
 
     static {
-        standartWattages.put("connectorType=1", "20 20 40");}
+        standartWattages.put("connectorType=4", "18 36 58"); //g13
+        standartWattages.put("connectorType=10", "35 70 150"); //g12
+        standartWattages.put("connectorType=18", "20 20 40");//g5
+        standartWattages.put("connectorType=11", "35 70 150");//rx7s
+        standartWattages.put("connectorType=1, type2=1", "25, 40, 60, 75");
 
-    private Item findStandartWattages(Itesm item)
-    {
+        private Item findStandartWattages (Item item)
+        {
 
+        }
     }
-} 
