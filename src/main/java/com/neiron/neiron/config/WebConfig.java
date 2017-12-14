103d32d174c5d1a5960dce57abdada304c2ddf4f
d.dyachkov <YVU7ih5G>
2017-12-14 14:44:38 +0300

    dsfds

--- src/main/java/com/neiron/neiron/config/WebConfig.java
+++ src/main/java/com/neiron/neiron/config/WebConfig.java
@@ -56,17 +56,17 @@ public class WebConfig  extends WebMvcConfigurerAdapter {
     public DriverManagerDataSource dataSource() {
 
         DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
-        driverManagerDataSource.setDriverClassName("org.postgresql.Driver");
-        driverManagerDataSource.setUrl("jdbc:postgresql://localhost:5432/dd");
-        driverManagerDataSource.setUsername("postgres");
-        driverManagerDataSource.setPassword("postgres");
+//        driverManagerDataSource.setDriverClassName("org.postgresql.Driver");
+//        driverManagerDataSource.setUrl("jdbc:postgresql://localhost:5432/dd");
+//        driverManagerDataSource.setUsername("postgres");
+//        driverManagerDataSource.setPassword("postgres");
         logger.error("DATA SOTCE INITIALIZED");
 
 
-//        driverManagerDataSource.setDriverClassName("org.postgresql.Driver");
-//        driverManagerDataSource.setUrl("jdbc:postgresql://ec2-107-22-165-47.compute-1.amazonaws.com:5432/d726pn04csmqqc");
-//        driverManagerDataSource.setUsername("vhfaivnimxdhsl");
-//        driverManagerDataSource.setPassword("375a8eeff896bb7a74202c45d188ce27a2272c534a229322579cc6f4af2e6ec3");
+        driverManagerDataSource.setDriverClassName("org.postgresql.Driver");
+        driverManagerDataSource.setUrl("jdbc:postgresql://ec2-107-22-165-47.compute-1.amazonaws.com:5432/d726pn04csmqqc");
+        driverManagerDataSource.setUsername("vhfaivnimxdhsl");
+        driverManagerDataSource.setPassword("375a8eeff896bb7a74202c45d188ce27a2272c534a229322579cc6f4af2e6ec3");
         return driverManagerDataSource;
     }
 
